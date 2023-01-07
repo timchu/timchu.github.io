84da61267c40405899397841101ec3815b5b5fc3
Timothy Chu <timchu@google.com>
2023-01-07 04:37:18 +0000

    Changed site up to have different side bar

---
+++ script.js
@@ -0,0 +1,23 @@
+function imgHtml(src) {
+  return '<div><br><div class="image-container"><div class="close-button" onclick=removeImage(this)>X</div><img src="' +
+      src + '"></div></div>';
+};
+
+function showImage(imgId) {
+  var sidePanel = document.getElementById('side-panel')
+  var src = document.getElementById(imgId).src;
+  sidePanel.innerHTML = imgHtml(src) + sidePanel.innerHTML;
+};
+
+function showCaptioned(captionedId) {
+  var sidePanel = document.getElementById('side-panel')
+  var captioned = document.getElementById(captionedId);
+  console.log(captioned)
+  sidePanel.innerHTML =
+      '<div><div class="captioned"><br><div class="close-button" onclick=removeImage(this)>X</div>' +
+      captioned.cloneNode(captioned).outerHTML + '</div><br/></div>' +
+      sidePanel.innerHTML;
+};
+function removeImage(element) {
+  element.parentNode.parentNode.remove();
+};
